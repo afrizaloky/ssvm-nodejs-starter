fb19251902e30c79483f77bfaa18f39606140a6e
afrizaloky <afrizaloky@outlook.com>
2020-09-24 16:26:50 +0700

    change isprime func

--- node/app.js
+++ node/app.js
@@ -1,4 +1,4 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const { say, is_prime } = require('../pkg/ssvm_nodejs_starter_lib.js');
 
 const http = require('http');
 const url = require('url');
@@ -7,10 +7,10 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
+  if (!queryObject['number']) {
+    res.end(`Please use command curl http://${hostname}:${port}/?number=number \n`);
   } else {
-    res.end(say(queryObject['name']) + '\n');
+    res.end(is_prime(parseInt(queryObject['number'])) + '\n');
   }
 });
 
