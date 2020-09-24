29bb23f3969adbb36711cf0bb200f6491db035b5
afrizaloky <afrizaloky@outlook.com>
2020-09-24 16:27:02 +0700

    add isprime func

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+// use ndarray::arr2;
 
 #[wasm_bindgen]
 pub fn say(s: &str) -> String {
@@ -6,3 +7,29 @@ pub fn say(s: &str) -> String {
   let r = String::from("hello ");
   return r + s;
 }
+
+
+#[wasm_bindgen]
+pub fn is_prime(n: u32) -> String {
+  if n <= 1 {
+      return "false".to_string();
+  }
+  for a in 2..n {
+      if n % a == 0 {
+          return "false".to_string(); // if it is not the last statement you need to use `return`
+      }
+  }
+  "true".to_string() // last value to return
+}
+
+// #[wasm_bindgen]
+// pub fn multiply_arr() {
+//     let a = arr2(&[[1, 2, 3],
+//                    [4, 5, 6]]);
+
+//     let b = arr2(&[[6, 3],
+//                    [5, 2],
+//                    [4, 1]]);
+
+//     println!("{}", a.dot(&b));
+// }
